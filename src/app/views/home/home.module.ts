144acfbbe2e6973ccb2e3f213503ece45c352275
dongmingming <dongmingming@renrenche.com>
2018-08-25 19:09:48 +0800

    list

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -4,13 +4,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 import { SharedModule } from '../../module/shared.module';
-import { ToolbarComponent } from './toolbar/toolbar.component'
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ListComponent } from './list/list.component'
 
 @NgModule({
   imports: [
     SharedModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent, ToolbarComponent]
+  declarations: [HomeComponent, ToolbarComponent, ListComponent]
 })
 export class HomeModule { }
