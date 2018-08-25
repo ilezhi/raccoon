5661d81fdf8decdea0f93500c34759ec205f2b0a
dongmingming <dongmingming@renrenche.com>
2018-08-25 17:50:59 +0800

    分页

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
-import { SharedModule } from '../../module/shared.module'
+import { SharedModule } from '../../module/shared.module';
+import { ToolbarComponent } from './toolbar/toolbar.component'
 
 @NgModule({
   imports: [
     SharedModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, ToolbarComponent]
 })
 export class HomeModule { }
