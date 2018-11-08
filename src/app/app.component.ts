b72236c9c2cba2aadc74e0833a090819217e0673
dongmingming <dongmingming@renrenche.com>
2018-11-08 18:30:07 +0800

    feat(progress): 显示路由进度

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core'
-import { Store } from '@ngrx/store'
+import { Store, select } from '@ngrx/store'
 
-import { getState } from 'src/app/reducers'
+import { getState, getUrl } from 'src/app/reducers'
+import { Router, NavigationStart, NavigationEnd } from '@angular/router'
+
+import NProgress from 'nprogress'
+
+NProgress.configure({ showSpinner: false })
 
 @Component({
   selector: 'app-root',
@@ -11,10 +16,20 @@ import { getState } from 'src/app/reducers'
 export class AppComponent {
   private name = 'app'
 
-  constructor(store: Store<any>) {
+  constructor(store: Store<any>, private router: Router) {
     store.select(getState)
       .subscribe(data => {
         console.log('app', data)
       })
+
+    this.router.events.subscribe(ev => {
+      if (ev instanceof NavigationStart) {
+        NProgress.start()
+      }
+
+      if (ev instanceof NavigationEnd) {
+        NProgress.done()
+      }
+    })
   }
 }
